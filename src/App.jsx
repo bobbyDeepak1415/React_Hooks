7c1d20e70e187bb70b62f68d602c146040ecbf54
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-07 23:51:49 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,16 @@ function App() {
   const [count, setCount] = useState(0);
   const [item, setItem] = useState("");
 
- 
+  const countFromChild = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
   return (
     <>
       <div>
         <h1>Parent count at:{count}</h1>
         <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
         <input onChange={(e) => setItem(e.target.value)} value={item} />
-        <Child updateCounter={()=>setCount(count+1)} count={count} />
+        <Child updateCounter={countFromChild} count={count} />
       </div>
     </>
   );
