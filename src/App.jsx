dcd33c48143d3633c7028331a71a43542028d6d7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-07 15:54:09 +0530

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 // import './App.css'
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import Child from "./Child";
 
 function App() {
   console.log("Parent rendering...");
 
   const [count, setCount] = useState(0);
-  const [item,setItem]=useState("")
+  const [item, setItem] = useState("");
+
+  const memoizedFuntionFromChild = useCallback(
+    () => setCount(count + 1),
+    [count],
+  );
 
   return (
     <>
       <div>
         <h1>Parent count at:{count}</h1>
-        <button onClick={() => setCount((prev) => prev + 1)}>Click</button>
-        <input value={item}/>
-        <Child count={count}/>
+        <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+        <input onChange={(e) => setItem(e.target.value)} value={item} />
+        <Child updateCounter={memoizedFuntionFromChild} count={count} />
       </div>
     </>
   );
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,11 +1,12 @@
 import React, { memo } from "react";
 
-const Child = ({count}) => {
+const Child = ({ count,updateCounter }) => {
   console.log("child rendering...");
 
   return (
     <div>
       <h1>Child count at:{count}</h1>
+      <button onClick={updateCounter}>Click</button>
     </div>
   );
 };
