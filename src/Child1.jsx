15cf861c30eea4b868f0c07061c7622eb0318aa1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-08 17:03:05 +0530

    setup done

---
+++ src/Child1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Child1 = () => {
+
+    console.log("child rendering...")
+
+  return (
+    <div>
+      <h1>Child count is at:{}</h1>
+    </div>
+  )
+}
+
+export default Child1
---
+++ src/Demo.jsx
@@ -0,0 +1,26 @@
+import React, { useMemo, useState } from "react";
+import Child1 from "./Child1";
+
+const Demo = () => {
+  console.log("Demo rendering...");
+
+  const [count, setCount] = useState(0);
+
+  const expensiveValue = useMemo(() => {
+    console.log("expensive function run...");
+    return count * 100;
+  }, [count]);
+
+  return (
+    <div>
+      <h1>Demo count is at :{count}</h1>
+      <h2>Expensive value:{expensiveValue}</h2>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <div>
+        <Child1 />
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
