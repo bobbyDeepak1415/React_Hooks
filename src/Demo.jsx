e5491ca1c359dab8cb9bc6295348e596f7a7c02b
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-09 00:50:09 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from "react";
 
 const Demo = () => {
+  console.log("parent rendering...");
 
-  const [count,setCount]=useState(0)
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState("");
 
+  // const expensiveResult = useMemo(() => {
+  //   return count * 100;
+  // }, [count]);
 
+  const expensiveResult = count * 100;
 
   return (
     <div>
       <h1>Parent count is at:{count}</h1>
+      <h2>Expensive Result is :{expensiveResult}</h2>
+      <input
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        type="text"
+      />
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
