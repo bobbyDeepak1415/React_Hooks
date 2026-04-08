8aabfab96cf4a1ef8293d1ae10c4fe4150f8ce13
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-09 03:24:44 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Child1 = () => {
+const Child1 = ({ count, updatedValue }) => {
+  console.log("child rendering...");
+
   return (
     <div>
-      
+      <h1>Child count is at:{count}</h1>
+      <button onClick={updatedValue}>Child increment</button>
     </div>
-  )
-}
+  );
+};
 
-export default Child1
+export default Child1;
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,18 +1,25 @@
-
-
-import React, { useState } from 'react'
+import React, { useCallback, useState } from "react";
+import Child1 from "./Child1";
 
 const Demo = () => {
+  const [count, setCount] = useState(0);
 
-  const [count,setCount]=useState(0)
+  console.log("parent rendering...");
+
+  const memoizedValue = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
 
   return (
     <div>
       <h1>Parent count is at:{count}</h1>
-      <button onClick={()=>setCount(prev=>prev+1)}>Increment</button>
-      
-    </div>
-  )
-}
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
 
-export default Demo
+      <div>
+        <Child1 count={count} updatedValue={memoizedValue} />
+      </div>
+    </div>
+  );
+};
+
+export default Demo;
