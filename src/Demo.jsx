174de1fd4ca5b74504c65a4cd712cf9e1e1c8588
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-09 03:17:36 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,10 +1,15 @@
 
 
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo = () => {
+
+  const [count,setCount]=useState(0)
+
   return (
     <div>
+      <h1>Parent count is at:{count}</h1>
+      <button onClick={()=>setCount(prev=>prev+1)}>Increment</button>
       
     </div>
   )
