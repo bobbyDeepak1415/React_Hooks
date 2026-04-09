9df45ae43c3483bbb01723a71be6cf4e0b02fc86
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-10 00:53:22 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
 const Child1 = () => {
+
+  console.log("child rendering...")
+
   return <div>Child1</div>;
 };
 
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,17 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import Child1 from "./Child1";
 
 const Demo = () => {
+  console.log("parenet rendering...");
 
-  console.log("parenet rendering...")
-
-  const [count,setCount]=useState(0)
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState("");
 
   return (
     <div>
       <h1>parent count is at:{count}</h1>
-    
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <input value={item} onChange={(e) => setItem(e.target.value)} />
+      <Child1 />
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
