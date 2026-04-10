7f9805f279fd4f29e895e64aaad1c4272ef59b35
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-11 00:20:25 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Demo = () => {
+
+  const [count,setCount]=useState(0)
+
   return (
     <div>
-      
+      <h1>count is now :{count}</h1>
+      <button onClick={()}>-</button>
+      <button onClick={}>+</button>
+      <button onClick={}>Reset</button>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
