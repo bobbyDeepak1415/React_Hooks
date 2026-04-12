c3fc5a1b20b2ed3aa5f5fc529d165c29e081b4f2
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-12 15:35:55 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Child1 = () => {
   return (
     <div>
-      
+      <h1>Child</h1>
     </div>
   )
 }
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import Child1 from "./Child1";
+import { handleClick } from "./handleClick";
 
 const Demo = () => {
+  const [title, setTitle] = useState("");
+
   return (
     <div>
-      
+      <h1>Demo</h1>
+      <h2>Name:{title}</h2>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <button
+        onClick={() => {
+          return <p>{title}</p>;
+        }}
+      >
+        Add
+      </button>
+      <Child1 />
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
