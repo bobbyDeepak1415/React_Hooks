42c11273eadea1421e9a1f78eea887666861ab08
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-12 15:30:48 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,22 +1,11 @@
-import React, { memo, useState } from "react";
+import React from 'react'
 
-const Child1 = ({ count, memoizedFunc }) => {
-  console.log("Child rendering...");
-
-  const [num,setNum]=useState(0)
-
+const Child1 = () => {
   return (
     <div>
-      <h1>Child Count is at:{count}</h1>
-
-      <h2>num:{num}</h2>
-      <button onClick={()=>setNum(num+1)}>AddOne</button>
-      <button onClick={memoizedFunc}>Child Incre</button>
-
-
-
+      
     </div>
-  );
-};
+  )
+}
 
-export default memo(Child1);
+export default Child1
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,37 +1,11 @@
-import React, { useCallback, useMemo, useState } from "react";
-import Child1 from "./Child1";
+import React from 'react'
 
 const Demo = () => {
-  console.log("Parent rendering...");
-
-  const [count, setCount] = useState(0);
-  const [item, setItem] = useState("");
-
-  const memoizedFunc = useCallback(() => {
-    console.log("expensive func run..");
-    setCount(count + 1);
-  }, [count]);
-
-  const expensiveValue = useMemo(() => {
-    console.log("expensive value computed...");
-    return count * 100;
-  }, [count]);
-
   return (
     <div>
-      <h1>Parent Count is at:{count}</h1>
-      <h2>Expensive value:{expensiveValue}</h2>
-      <button onClick={memoizedFunc}>Increment</button>
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <input value={item} onChange={(e) => setItem(e.target.value)} />
-      </div>
-      <Child1 memoizedFunc={memoizedFunc} count={count} />
+      
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
