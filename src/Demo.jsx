a33ee25611245f741a3f86f323c05c5c13e5d78c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-17 12:40:04 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Demo = () => {
-  return <div>Hello</div>;
+  const [count, setCount] = useState(0);
+  const [nums, setNums] = useState([]);
+
+  return (
+    <div>
+      <h2>Count is now at:{count}</h2>
+      <button
+        onClick={() => {
+          setNums([...nums, count]);
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Increment
+      </button>
+
+      <div>
+        {nums.map((num) => {
+          return <p>{num}</p>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
