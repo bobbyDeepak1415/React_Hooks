2ea65977b91677f422d5c6c03c452923cb2a9884
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-18 01:14:17 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -4,8 +4,13 @@ const initialState = {
   count: 0,
 };
 
-function reducer(){
-  
+function reducer(state,action){
+  switch(action.type){
+    case "increment" : return {count:state.count+1}
+    case "decrement" : return {count:state.count-1}
+    case "reset" : return {count:0}
+  }
+
 }
 
 const Demo = () => {
