677604e273c9fde5515d252ef703414a99016738
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-04-18 01:04:45 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,31 +1,22 @@
-import React, { useReducer } from "react";
-
-const initialState = {
-  count: 0,
-};
-
-function reducer(state, action) {
-  if (action.type === "increment") {
-    return {
-      count: state.count + 1,
-    };
-  } else {
-    return {
-      count: state.count - 1,
-    };
-  }
-}
+import React, { useState } from 'react'
 
 const Demo = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const [count,setCount]=useState(0)
+  const [nums,setNums]=useState([])
 
   return (
     <div>
-      <h1>Count is at:{state.count}</h1>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <p>count at: {count}</p>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <div>
+        {nums.map((num)=>{
+          return <li>{num}</li>
+        })}
+      </div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
