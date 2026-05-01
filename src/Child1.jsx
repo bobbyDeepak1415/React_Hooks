dd055258e83ebab54a8ef826908ff60ce304faa7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-01 18:09:05 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Child1 = () => {
-  return <div></div>;
+const Child1 = ({count}) => {
+
+  console.log('child rendering...')
+  return <div>
+
+
+    <h1>Child count is at:{count}</h1>
+  </div>;
 };
 
 export default Child1;
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Child1 from './Child1'
 
 const Demo = () => {
+
+  console.log("parent rendering...")
+  const [item,setItem]=useState("")
+
+  const [count,setCount]=useState(0)
   return (
     <div>
-      Hello
+
+      <h1>The count is at :{count}</h1>
+      <button onClick={()=>setCount(prev=>prev+1)}>Increment</button>
+      <div>
+
+      <input value={item} onChange={(e)=>setItem(e.target.value)}/>
+      </div>
+      <div>
+        <Child1 count={count}/>
+      </div>
+      
     </div>
   )
 }
