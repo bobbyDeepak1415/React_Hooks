b83242df04a6a23502f144c8c949219e8baa69bf
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-01 18:18:48 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,5 +1,6 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import Child1 from "./Child1";
+import handleClick from "./handleClick";
 
 const Demo = () => {
   console.log("parent rendering...");
@@ -8,14 +9,21 @@ const Demo = () => {
   const [count, setCount] = useState(0);
 
   const expensiveFunc = useCallback(() => {
-    console.log('expensive func reunning...')
-    setCount((prev) => prev + 19);
+    console.log("expensive func reunning...");
+    handleClick(setCount);
   }, []);
+
+  const memoizedValue = useMemo(() => {
+    console.log("expensive value...");
+
+    return count * 1000;
+  }, [count]);
 
   return (
     <div>
       <h1>The count is at :{count}</h1>
       <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+      <h2>Memoized Value:{memoizedValue}</h2>
       <div>
         <input value={item} onChange={(e) => setItem(e.target.value)} />
       </div>
--- src/handleClick.jsx
+++ src/handleClick.jsx
@@ -1,6 +1,6 @@
-const handleClick = () => {
+const handleClick = (setCount) => {
+setCount(prev=>prev+19)
 
-    
 };
 
 export default handleClick;
