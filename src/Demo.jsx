9f6b12583131a255029baffb44f94b208a981be0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-02 02:35:43 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -5,7 +5,13 @@ const Demo = () => {
   const count=useRef(0)
   return <div>
 
+{/* <h2>Count is at:{</h2> */}
+<button  onClick={()=>{
 
+  console.log(count.current)
+  count.current+=1
+}
+}>Click</button>
 
   </div>;
 };
