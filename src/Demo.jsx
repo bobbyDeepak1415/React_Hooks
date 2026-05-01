1f6b5f920ac45269c285af0fea17a68fce37a206
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-02 02:42:01 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,19 +1,17 @@
-import React, { useRef } from "react";
+import React, { useRef } from 'react'
 
 const Demo = () => {
 
   const count=useRef(0)
-  return <div>
 
-{/* <h2>Count is at:{</h2> */}
-<button  onClick={()=>{
-
-  console.log(count.current)
-  count.current+=1
+  return (
+    <div>
+      <button onClick={()=>{
+        count.current=count.current+1
+        console.log(count.current)
+      }}>Click</button>
+    </div>
+  )
 }
-}>Click</button>
 
-  </div>;
-};
-
-export default Demo;
+export default Demo
