2912661591cad55168a9d53c5993abe0256c051a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-01 18:26:19 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,13 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// // import './index.css'
-// // import App from './App.jsx'
-// import Demo from './Demo.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+import Demo from './Demo.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     {/* <App /> */}
-//     <Demo/>
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <Demo/>
+  </StrictMode>,
+)
 
-let str = "AAABBCCADDDDBA";
-
-function display(str) {
-  let result = "";
-  let obj = {};
-  for (let i of str) {
-    obj[i] = (obj[i] || 0) + 1;
-  }
-
-  let seen = new Set();
-
-  for (let i of str) {
-    if (!seen.has(i)) {
-      result += i + obj[i];
-      seen.add(i);
-    }
-  }
-
-  return result;
-}
-
-console.log(display(str));
