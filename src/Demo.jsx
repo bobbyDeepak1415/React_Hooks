e0f81c88447815520f89532c2ab8c3d7c039e34f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-05 18:26:09 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,37 +1,11 @@
-import React, { useReducer } from "react";
-
-
-
-const initialState={
-  count:0
-}
-
-function reducer(state,action){
-
-  switch(action.type){
-    case "increment" :
-      return {count:state.count+1}
-    case "decrement" :
-      return {count:state.count-1}
-    case "reset" :
-      return {count:0}
-  }
-
-}
+import React from 'react'
 
 const Demo = () => {
-
-  const [state,diapatch]=useReducer(reducer,initialState)
-
-
   return (
     <div>
-      <h2>count is at:{state.count}</h2>
-      <button onClick={()=>diapatch({type:"decrement"})}>-</button>
-      <button onClick={()=>diapatch({type:"increment"})}>+</button>
-      <button onClick={()=>diapatch({type:"reset"})}>reset</button>
+      <h2>Count is at:</h2>
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-import Demo from './Demo.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// // import './index.css'
+// // import App from './App.jsx'
+// import Demo from './Demo.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-    <Demo/>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     {/* <App /> */}
+//     <Demo/>
+//   </StrictMode>,
+// )
 
+let str = "AADDBBDDACC";
+
+function display(str) {
+  let obj = {};
+  let result = "";
+  for (let i of str) {
+    obj[i] = (obj[i] || 0) + 1;
+  }
+
+  let seen=new Set()
+
+  for(let i of str){
+    if(!seen.has(i)){
+      result+=i+obj[i]
+      seen.add(i)
+    }
+  }
+
+  return result
+
+}
+
+console.log(display(str))
