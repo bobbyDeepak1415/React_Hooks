cd25fcc5dc1ea270a3ffd43be3b0e83ba08ac761
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-20 11:45:34 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import useFetchProds from "./useFetchProds";
 
 const Demo = () => {
+  const {
+    data: products,
+    loading,
+    error,
+  } = useFetchProds("https://dummyjson.com/products");
+
+  if (loading) {
+    return <div>Loading product titles...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div>
-      <h2>Count is at:</h2>
+      <h2>Product Titles</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Demo
