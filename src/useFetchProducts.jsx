b58bda2accd1295716bb3301b1fba03f9f0f5c48
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-20 11:46:12 +0530

    setup done

---
+++ src/useFetchProducts.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+function useFetchProds(url) {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setData(result.products || []);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetchProds;
