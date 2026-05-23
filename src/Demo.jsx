3a09cdb98f32e25af7e5ecbdad9d6b7dd6357fd0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-23 16:02:30 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,40 +1,36 @@
 import React, { useState } from "react";
 import useFetchProds from "./useFetchProds";
 
-let url = "https://dummyjson.com/products";
-
 const Demo = () => {
-  const { products } = useFetchProds(url);
-
   const [status, setStatus] = useState("all");
 
-  const uniqueCategories = Array.from(
-    new Set(products.map((prod) => prod.category)),
-  );
+  const { products } = useFetchProds("https://dummyjson.com/products");
 
-  const filteredProds =
+  const filteredProducts =
     status === "all"
       ? products
       : products.filter((prod) => prod.category === status);
 
+  const categories = Array.from(new Set(products.map((prod) => prod.category)));
+
   return (
     <div>
-      <h2>
-        Products here:
-        <section>
-          <select onChange={(e) => setStatus(e.target.value)}>
-            <option>all</option>
-            {uniqueCategories.map((i) => {
-              return <option value={i}>{i}</option>;
-            })}
-          </select>
-          <ul>
-            {filteredProds.map((item) => {
-              return <li>{item.title}</li>;
-            })}
-          </ul>
-        </section>
-      </h2>
+      <h2>Item List:</h2>
+
+      <section>
+        <select onChange={(e) => setStatus(e.target.value)}>
+          <option>all</option>
+          {categories.map((i) => {
+            return <option value={i}>{i}</option>;
+          })}
+        </select>
+
+        <ul>
+          {filteredProducts.map((prod) => {
+            return <li>{prod.title}</li>;
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
