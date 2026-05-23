d56fc517729d18bb8c7f4af81e64b3365a439c95
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-05-24 03:02:22 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,38 +1,14 @@
-import React, { useState } from "react";
-import useFetchProds from "./useFetchProds";
+import React, { createContext } from 'react'
 
 const Demo = () => {
-  const [status, setStatus] = useState("all");
 
-  const { products } = useFetchProds("https://dummyjson.com/products");
-
-  const filteredProducts =
-    status === "all"
-      ? products
-      : products.filter((prod) => prod.category === status);
-
-  const categories = Array.from(new Set(products.map((prod) => prod.category)));
+  const nameContext=createContext()
 
   return (
     <div>
-      <h2>Item List:</h2>
-
-      <section>
-        <select onChange={(e) => setStatus(e.target.value)}>
-          <option>all</option>
-          {categories.map((i) => {
-            return <option value={i}>{i}</option>;
-          })}
-        </select>
-
-        <ul>
-          {filteredProducts.map((prod) => {
-            return <li>{prod.title}</li>;
-          })}
-        </ul>
-      </section>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// // import './index.css'
-// // import App from './App.jsx'
-// import Demo from "./Demo.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
+import Demo from "./Demo.jsx";
 
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     {/* <App /> */}
-//     <Demo />
-//   </StrictMode>,
-// );
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    {/* <App /> */}
+    <Demo />
+  </StrictMode>,
+);
+
+
+
