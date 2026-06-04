8e9ca5cd01e39dae91052e46aa232b5d56213f3f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-04 17:53:21 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,6 +1,30 @@
+import { useReducer } from "react";
+
+const initialState = {
+  count: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return (state.count += 1);
+    case "decrement":
+      return (state.count -= 1);
+
+    case "reset":
+      return (state.count = 0);
+  }
+}
+
 const Demo = () => {
-  return <div></div>;
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <p>{count.current}</p>
+      <button onClick={() => dispatch({type:"increment"})}>+</button>
+    </div>
+  );
 };
 
 export default Demo;
-...
