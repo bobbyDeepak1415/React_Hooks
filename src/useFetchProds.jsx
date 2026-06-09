157f3ba3b7bc65dcb62453063a95c2657531fc29
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-10 02:58:58 +0530

    setup done

--- src/useFetchProds.jsx
+++ src/useFetchProds.jsx
@@ -4,22 +4,33 @@ import React, { useEffect, useState } from "react";
 const useFetchProds = (url) => {
   const [products, setProducts] = useState([]);
 
+  const MAX_TRIES=3
+
   useEffect(() => {
-    const fetchData = async () => {
-      axios
-        .get(url)
-        .then((res) => {
-          setProducts(res.data.products);
-        })
-        .catch((err) => {
-          console.log("failed to fetch", err);
-        });
+    const fetchData = async (triesLeft) => {
+      for(let i=0;i<triesLeft;i++){
+  if(triesLeft<3){
+    console.log(`failed to fetch, triesLeft=${triesLeft-1}`)
+    fetchData(triesLeft-1)
+  }else if(triesLeft>=3){
+
+  }
+      }
     };
-
-    fetchData();
+    
+    fetchData(MAX_TRIES);
   }, []);
-
+  
   return { products };
 };
 
 export default useFetchProds;
+
+// await axios
+//   .get(url)
+//   .then((res) => {
+//     setProducts(res.data.products);
+//   })
+//   .catch((err) => {
+//     console.log("failed to fetch", err);
+//   });
