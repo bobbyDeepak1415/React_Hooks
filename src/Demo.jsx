80d30abd987e52e8c3e8e1dccab679b43e1c6749
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-10 15:55:34 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import useFetchProds from "./useFetchProds";
 
 const Demo = () => {
--- src/useFetchProds.jsx
+++ src/useFetchProds.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-const useFetchProds = () => {
-  return <div></div>;
+const useFetchProds = (url) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        axios.get(url).then((res) => {
+          setProducts(res.data.products || res.data);
+        });
+      } catch (err) {
+        console.log("failed to fetch", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { products };
 };
 
 export default useFetchProds;
