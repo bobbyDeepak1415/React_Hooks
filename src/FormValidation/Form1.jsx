9df89a28e4016958cdddb244bb9ada53ab7c5099
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-20 02:52:43 +0530

    setup done

--- src/FormValidation/Form1.jsx
+++ src/FormValidation/Form1.jsx
@@ -3,23 +3,54 @@ import React, { useState } from "react";
 const Form1 = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
 
+  const [errors, seterrors] = useState({ email: "", password: "" });
 
-  const handleChange=()=>{
+
+  const validate=(name,value)=>{
+    let errorMsg=""
+
+    if(name==="email"){
+      if(!value.includes("@")){
+        errorMsg="Email must contain an @ symbol"
+      }
+    }
     
+    if(name==="password"){
+      if(!value.length>6){
+        errorMsg="password must be at least 6 characters long"
+      }
+    }
+    
+
   }
 
-  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+
+    validate(name,value)
+
+  };
+
   return (
     <div>
       <form>
         <label>Email:</label>
         <input onChange={handleChange} name="email" value={formData.email} />
+        {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
         <label>Password:</label>
         <input
           onChange={handleChange}
           name="password"
           value={formData.password}
         />
+        {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
       </form>
     </div>
   );
