0d73117535157edf3e474777378046a7b00e8e1f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-20 00:11:02 +0530

    setup done

--- src/FormValidation/Form1.jsx
+++ src/FormValidation/Form1.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Form1 = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
   return (
     <div>
-      
+      <form>
+        <label>Email:</label>
+        <input value={formData.email}></input>
+        <label>Password:</label>
+        <input value={formData.password}></input>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form1
+export default Form1;
