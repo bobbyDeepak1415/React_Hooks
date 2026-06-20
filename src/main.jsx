8951adb19ef42b0edfce44eed188930525380454
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-20 16:10:52 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 // import Demo from "./Demo.jsx";
-import Form1 from "./FormValidation/Form1.jsx";
+// import Form1 from "./FormValidation/Form1.jsx";
+import Form2 from "./FormValidation/Form2.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     {/* <Demo /> */}
-    <Form1/>
+    <Form2/>
   </StrictMode>,
 );
 
