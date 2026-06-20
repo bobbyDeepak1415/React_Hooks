18600a13bccf8814f0fd02f5ffe91f6e849a52f0
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-20 16:39:44 +0530

    setup done

--- src/FormValidation/Form2.jsx
+++ src/FormValidation/Form2.jsx
@@ -3,32 +3,6 @@ import React, { useState } from "react";
 const Form2 = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
 
-  const [errors, setErrors] = useState({ email: "", password: "" });
-
-  const [touched,setTouched]=useState({email:false,password:false})
-
-  const validate = (name, value) => {
-    let errorMsg = "";
-
-    if (name === "email") {
-      if (!value.includes("@")) {
-        errorMsg = "email must contain @";
-      }
-    }
-    if (name === "password") {
-      if (!value.length > 6) {
-        errorMsg = "password must be atleast 6 characters long";
-      }
-    }
-
-    setErrors((prev) => {
-      return {
-        ...prev,
-        [name]: errorMsg,
-      };
-    });
-  };
-
   const handleChange = (e) => {
     const { name, value } = e.target;
 
@@ -38,30 +12,12 @@ const Form2 = () => {
         [name]: value,
       };
     });
-
-    validate(name, value);
   };
-
-  const handleBlur=(e)=>{
-
-    const {name}=e.target
-
-    setTouched((prev)=>{
-        return {
-            ...prev,
-            [name]:true
-        }
-    })
-
-
-
-  }
 
   return (
     <div>
       <label>Email:</label>
-      <input onBlur={handleBlur} value={formData.email} name="email" onChange={handleChange} />
-      {errors.email && <p style={{color:"red"}}>{errors.email}</p>}
+      <input name="email" value={formData.email} onChange={handleChange} />
     </div>
   );
 };
