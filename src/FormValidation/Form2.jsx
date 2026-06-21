2568ef9050dc4d24975e57928c6fe7e4570d0fd3
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-21 12:32:52 +0530

    setup done

--- src/FormValidation/Form2.jsx
+++ src/FormValidation/Form2.jsx
@@ -1,29 +1,73 @@
 import React, { useState } from "react";
 
 const Form2 = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
 
-  const [formData,setFormData]=useState({email:"",password:""})
+  const [error, setError] = useState({ email: "", password: "" });
 
-  const handleChange=(e)=>{
-const {name,value}=e.target
+  const [touched, setTouched] = useState({ email: false, password: false });
 
-setFormData((prev)=>{
-  return {
-    ...prev,
-    [name]:value
-  }
-})
+  const validate = (name, value) => {
+    let errorMsg = "";
 
-  }
+    if (name === "email") {
+      if (!value.includes("@")) {
+        errorMsg = "the email must contain @";
+      }
+    }
 
+    if (name === "password") {
+      if (value.length < 6) {
+        errorMsg = "the password must be atleast 6 characters long";
+      }
+    }
 
-  return <div>
+    setError((prev) => {
+      return {
+        ...prev,
+        [name]: errorMsg,
+      };
+    });
+  };
 
-    <form>
-      <label>Email:</label>
-      <input value={formData.email} onChange={handleChange}/>
-    </form>
-  </div>;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+
+    validate(name, value);
+  };
+
+  const handleBlur = (e) => {
+    const { name } = e.target;
+
+    setTouched((prev) => {
+      return {
+        ...prev,
+        [name]: true,
+      };
+    });
+  };
+
+  return (
+    <div>
+      <form>
+        <label>Email:</label>
+        <input
+          // onBlur={handleBlur}
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        {touched.email && error.email && <p style={{ color: "red" }}>{error.email}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default Form2;
