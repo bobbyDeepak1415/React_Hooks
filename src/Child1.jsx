2de71cc1028a0b685676eefb5f9da81d09988e7f
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-06-26 16:26:36 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Child1 = () => {
-
-  const [count,setCount]=useState(0)
-
-  useEffect(()=>{
-    const getId=setInterval(()=>{
-setCount(count+1)
-
-return ()=>clearInterval(getId)
-
-    },1000)
-  })
-
   return (
     <div>
-
-      <p>{count}</p>
-      
+      Hello
     </div>
   )
 }
