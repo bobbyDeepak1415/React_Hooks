ef0f5b27801f85a9d902ca53a6c23417256c8ae7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-08 16:12:34 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -4,36 +4,14 @@ const initialState = {
   count: 0,
 };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "increment":
-      return {
-        count: state.count + 1,
-      };
-
-    case "decrement": {
-      if (state.count <= 0) return;
-      return {
-        count: state.count - 1,
-      };
-    }
-    case "reset": {
-      return {
-        count: 0,
-      };
-    }
-  }
-}
-
 const Child1 = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+
+
+  const [count,dispatch]=useReducer()
 
   return (
     <div>
-      <h2>{state.count}</h2>
-      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
-      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+      <h2>{}</h2>
     </div>
   );
 };
