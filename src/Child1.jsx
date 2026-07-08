b556151aca9bec6c121290321ecd604e82e0a34a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-08 16:03:06 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
+
+
+const initialState={
+  count:1000
+}
+
+function reducer(state,action){
+
+  
+
+}
+
 
 const Child1 = () => {
 
-  const [count,setCount]=useState(0)
+  // const [count,setCount]=useState(0)
+
+  const [state,dispatch]=useReducer(reducer,initialState)
 
   const handleClick=()=>{
-    
+
   }
 
   return (
     <div>
-      <h2>{count}</h2>
+      <h2>{state.count}</h2>
       <button onClick={handleClick}>+</button>
     </div>
   )
