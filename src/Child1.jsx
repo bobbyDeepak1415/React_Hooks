e806590ab5187b6f285390cca8f9c2a2e27fa113
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-08 23:57:37 +0530

    setup done

--- src/Child1.jsx
+++ src/Child1.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 const Child1 = (props) => {
+
+    console.log("child rendering...")
+  
+
   return <div>
     <h3>Child count at:{props.count}</h3>
   </div>;
--- src/Demo.jsx
+++ src/Demo.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import Child1 from "./Child1";
 
 const Demo = () => {
+
+  console.log("parent rendering...")
   const [count, setCount] = useState(0);
+
+  const [input,setInput]=useState("")
 
   return (
     <div>
       <h1>Hello</h1>
+      <input value={input} onChange={(e)=>setInput(e.target.value)}/>
       <h2>{count}</h2>
       <button onClick={() => setCount((prev) => prev + 1)}>+</button>
       <Child1 count={count} />
