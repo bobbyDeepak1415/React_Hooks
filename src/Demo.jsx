366fecbb528dc6a940883de0a0a850e069609e52
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-17 12:56:36 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
-const Demo = () => {
-  return <div>Heloo0000...</div>;
-};
+import { useState } from "react";
 
-export default Demo;
+export default function Demo() {
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setItems([...items, input]);
+    setInput("");
+  };
+
+  
+  return (
+    <div>
+      <h2>To do list:</h2>
+
+      <input value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleClick}>Add</button>
+      <div>
+        <ol start={1}>
+          {items.map((item, id) => {
+            return (
+              <li draggable onDragEnd={} onDragStart={} onDragEnter={} onDragOver={()} key={id}>
+                <span>{item}</span>
+              </li>
+            );
+          })}
+        </ol>
+      </div>
+    </div>
+  );
+}
