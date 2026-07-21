c951ae235ab928bc5ef9b180a6b11ba70a1afea7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-22 00:05:27 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Demo = () => {
   return (
-    <div>
-      Hello
+    <div className="container">
+      <form>
+        <h1>Login form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>UserName</label>
+            <input type="text" name="username" placeholder="UserName" />
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input type="password" name="password" placeholder="Password" />
+          </div>
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
