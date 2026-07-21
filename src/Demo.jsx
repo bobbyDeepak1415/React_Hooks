b7549a49b75cbc74459a665aac9a103419dd5864
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-22 00:07:06 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -3,25 +3,7 @@ import React from "react";
 const Demo = () => {
   return (
     <div className="container">
-      <form>
-        <h1>Login form</h1>
-        <div className="ui divider"></div>
-        <div className="ui form">
-          <div className="field">
-            <label>UserName</label>
-            <input type="text" name="username" placeholder="UserName" />
-          </div>
-          <div className="field">
-            <label>Email</label>
-            <input type="email" name="email" placeholder="Email" />
-          </div>
-          <div className="field">
-            <label>Password</label>
-            <input type="password" name="password" placeholder="Password" />
-          </div>
-          <button className="fluid ui button blue">Submit</button>
-        </div>
-      </form>
+      
     </div>
   );
 };
