6cfad1a63eff9169ee77b2cfe9baa735a23dbd58
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-07-22 13:23:22 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,53 +1,8 @@
 import React, { useState } from "react";
 
 const Demo = () => {
-  const initialValues = { username: "", password: "", email: "" };
-
-  const [formValues, setFormValues] = useState(initialValues);
-
-  const handleChange = (e) => {
-    console.log(e.target.value);
-  };
-
-  return (
-    <div className="container bg-gray">
-      <form>
-        <h1>Login Form:</h1>
-        <div className="ui form">
-          <div className="ui field">
-            <label>UserName:</label>
-            <input
-              value={formValues.username}
-              type="text"
-              name="username"
-              placeholder="Username"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="ui field">
-            <label>Email:</label>
-            <input
-              onChange={handleChange}
-              value={formValues.email}
-              type="email"
-              name="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="ui field">
-            <label>Password:</label>
-            <input
-              onChange={handleChange}
-              type="password"
-              value={formValues.email}
-              name="password"
-              placeholder="Password"
-            />
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+  
+  
 };
 
 export default Demo;
