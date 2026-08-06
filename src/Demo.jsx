b3b26fb29334f8109419421b720a0cdd8cc5e242
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-07 01:42:28 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Demo = () => {
-  return <div>Hello</div>;
+
+const inputFileRef=useRef(0)
+
+  return <div>
+    <input type="file" ref={inputFileRef}/>
+    <button></button>
+    Your file List here:
+  </div>;
 };
 
 export default Demo;
