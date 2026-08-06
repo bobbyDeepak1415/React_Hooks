e718e8641f8967d6eea6ddee7c2befcae83b10ea
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-07 01:47:16 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,14 +1,33 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Demo = () => {
+  const inputFileRef = useRef(0);
+  const [files, setFiles] = useState([]);
 
-const inputFileRef=useRef(0)
+  const handleClick = () => {
+    inputFileRef.current.cick();
+  };
 
-  return <div>
-    <input type="file" ref={inputFileRef}/>
-    <button></button>
-    Your file List here:
-  </div>;
+  return (
+    <div>
+      <input
+        type="file"
+        hidden
+        ref={inputFileRef}
+        multiple
+        onChange={(e) => setFiles(e.target.files)}
+      />
+      <button onClick={handleClick}>Browse files</button>
+      <div>
+        <h2>Your file List here:</h2>
+        <ul>
+          {files.map((file, index) => {
+            return <li key={index}>{file}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Demo;
