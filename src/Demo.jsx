721a618afe481aff1da1e47bedc1b7f69dae5c65
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-09 23:55:46 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,33 +1,11 @@
-import React, { useRef, useState } from "react";
+import React from 'react'
 
 const Demo = () => {
-  const inputFileRef = useRef(0);
-  const [files, setFiles] = useState([]);
-
-  const handleClick = () => {
-    inputFileRef.current.cick();
-  };
-
   return (
     <div>
-      <input
-        type="file"
-        hidden
-        ref={inputFileRef}
-        multiple
-        onChange={(e) => setFiles(e.target.files)}
-      />
-      <button onClick={handleClick}>Browse files</button>
-      <div>
-        <h2>Your file List here:</h2>
-        <ul>
-          {files.map((file, index) => {
-            return <li key={index}>{file}</li>;
-          })}
-        </ul>
-      </div>
+      <input/>
     </div>
-  );
-};
+  )
+}
 
-export default Demo;
+export default Demo
--- src/useFetchProds.jsx
+++ src/useFetchProds.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const useFetchProds = () => {
+
+  const [input,setInput]=useState("")
+
   return (
     <div>
       Hello
