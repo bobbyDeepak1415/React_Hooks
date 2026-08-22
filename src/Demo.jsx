a479d05ae2bfb6ff1561c2500e8bc3d10d3f3562
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-22 23:55:35 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,20 +1,11 @@
-import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, multiplyByAmount } from "./redux/CounterSlice";
-
+import React from 'react'
 
 const Demo = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
 
-const dispatch = useDispatch();
-
-const count=useSelector(state=>state.counter.count)
-
-  return <div>
-
-    <p>count is at:{count}</p>
-    <button onClick={()=>dispatch(decrement())}>-</button>
-    <button onClick={()=>dispatch(increment())}>+</button>
-    <button onClick={()=>dispatch(multiplyByAmount(3))}>*3</button>
-  </div>;
-};
-
-export default Demo;
+export default Demo
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,10 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 import Demo from "./Demo.jsx";
-import { Provider } from "react-redux";
-import { store } from "./redux/Store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <Provider store={store}>
-
-
-    <Demo />
-    </Provider>
-    
-  </StrictMode>,
+<Demo />
+    </StrictMode>,
 );
