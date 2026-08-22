742de76565b9c7e05670d15ae1daeaa90a7275fe
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-22 23:54:18 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,12 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, multiplyByAmount } from "./redux/CounterSlice";
 
 
 const Demo = () => {
 
 const dispatch = useDispatch();
 
+const count=useSelector(state=>state.counter.count)
+
   return <div>
 
     <p>count is at:{count}</p>
+    <button onClick={()=>dispatch(decrement())}>-</button>
+    <button onClick={()=>dispatch(increment())}>+</button>
+    <button onClick={()=>dispatch(multiplyByAmount(3))}>*3</button>
   </div>;
 };
 
