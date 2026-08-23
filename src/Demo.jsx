2ecb2cdbe4fe0ea7192a9de104cc020f726244d4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-23 16:18:44 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Demo = () => {
+
+  const [count,setCount]=useState(0)
+
   return (
     <div>
-      Hello
+      <p>count is at :{count}</p>
     </div>
   )
 }
