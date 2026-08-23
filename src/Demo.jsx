81347a41c04dc4e8b362e9d943c160b5216b7d1c
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-23 16:30:19 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -8,13 +8,23 @@ const initialState={
 const reducerFun=(state,action)=>{
   switch(action.type){
     case "increment":
- return state.count+=1
+
+    return{
+
+      ...state,count:state.count+1
+    }
   case "decrement":
-   return state.count-=1
+   return{
+
+      count:state.count-1
+    }
   case "reset":
-   return state.count=0
-  case "default":
-   return state.count=0
+   return{
+
+      count:0
+    }
+  default:
+   return state
 
 
 
