cf9798bb78b15cb8db36a716770eebe26e2631e7
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-08-28 01:39:08 +0530

    setup done

--- src/Child.jsx
+++ src/Child.jsx
@@ -12,3 +12,13 @@ const Child = ({ count, updateCounter }) => {
 };
 
 export default memo(Child);
+
+
+// promise.all only resolves when all the promises resolve,even if a single promise fails instantly fails
+
+// promise.allSettled resolves after all the promises are completed whether resolved or rejected. it never fails
+
+
+// promise.any resolves as soon as the first promise resolves. it rejects only if all the promises are rejected.
+
+// promise.race resolves as soon as the first promise resolves. if the first promise fails it fails entirely
