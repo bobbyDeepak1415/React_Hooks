5369f72afea9486113b827f4328773187d7f91d1
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-02 13:06:22 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,20 +1,9 @@
-import React, { useReducer } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, multiplyByAmount } from './redux/NumbersSlice';
-
+import React from 'react'
 
 const Demo = () => {
-
-  const num=useSelector(state=>state.numbers.num)
-
-  const dispatch = useDispatch();
-
   return (
     <div>
-      <p>Count at:{num}</p>
-      <button onClick={()=>dispatch(decrement())}>Decrement</button>
-      <button onClick={()=>dispatch(increment())}>Increment</button>
-      <button onClick={()=>dispatch(multiplyByAmount(3))}>*3</button>
+      Hello bruv
     </div>
   )
 }
