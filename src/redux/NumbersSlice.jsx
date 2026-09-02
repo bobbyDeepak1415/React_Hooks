7b1709c80e0dc821b9d47b768529d8a3bf5c4878
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-02 12:57:48 +0530

    setup done

---
+++ src/redux/NumbersSlice.jsx
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    num:0
+}
+
+
+
+
+const NumbersSlice=createSlice({
+    name:"numbers",
+    initialState,
+    reducers:{
+        increment:()=>{
+            state.num+=1
+        },
+        decrement:()=>{
+            state.num-=1
+        }
+    }
+
+
+
+})
+
+export const {increment,decrement}=NumbersSlice.actions
