07ddf8e303c978bacf898c579a06f38ff43a58a5
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-03 23:55:24 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -6,17 +6,25 @@ const Demo = () => {
   const [price,setPrice]=useState(0)
   const [itemList,setItemList]=useState([])
 
+
+  const handleAddItem=(item,price)=>{
+    
+    setItemList([...itemList,item,price])
+
+
+  }
+
   return (
     <div>
       <h1>Groceries</h1>
       <input value={item} onChange={(e)=>setItem(e.target.value)}/>
       <input value={price} onChange={(e)=>setPrice(e.target.value)}/>
-      <button>AddToCart</button>
+      <button onClick={()=>handleAddItem(item,price)}>AddToCart</button>
 <div>
   <ul>
 
-  {itemList.map((item)=>{
-    return <li>{}</li>
+  {itemList.map((item,id)=>{
+    return <li key={id}>{item.name} <span>{item.price}</span></li>
   })}
   </ul>
 </div>
