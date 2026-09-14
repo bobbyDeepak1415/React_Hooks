bfa8694e3ae1d272021b5f355b2996b0df3d67a4
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-15 00:28:52 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,41 +1,10 @@
-import React, { useReducer } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { increment,decrement } from './redux/CounterSlice';
-
-
-const initialState={
-    count:20
-}
-
-
-function reducer(state,action){
-
-  switch(action.type){
-    case "increment":
-      return {
-        count:state.count+1
-      }
-    case "decrement":
-      return {
-        count:state.count-1
-      }
-
-  }
-
-}
-
+import React from 'react'
 
 const Demo = () => {
-
-
-  const [state,dispatch]=useReducer(reducer,initialState)
-
- return (
-  <div>
-  <h2>Count at:{state.count}</h2>
-  <button onClick={()=>dispatch({type:"decrement"})}>Decrement</button>
-  <button onClick={()=>dispatch({type:"increment"})}>increment</button>
-  </div>
+  return (
+    <div>
+      Hello
+    </div>
   )
 }
 
