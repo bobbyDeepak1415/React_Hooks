60369c6025e19ef65f485ab7b0abb83509ef5e99
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-15 00:26:37 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -12,7 +12,13 @@ function reducer(state,action){
 
   switch(action.type){
     case "increment":
-      return count=state.count+1
+      return {
+        count:state.count+1
+      }
+    case "decrement":
+      return {
+        count:state.count-1
+      }
 
   }
 
