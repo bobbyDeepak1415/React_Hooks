96bc860202b20023dbd8999e09850ad47d8f711a
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-15 15:10:49 +0530

    setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { Profiler, StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import './index.css'
-// import App from './App.jsx'
-import Demo from "./Demo.jsx";
+// import { Profiler, StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// // import './index.css'
+// // import App from './App.jsx'
+// import Demo from "./Demo.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* <App /> */}
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     {/* <App /> */}
       
 
-<Demo />
+// <Demo />
 
-    </StrictMode>,
-);
+//     </StrictMode>,
+// );
 
 
+
+let str='Hello World'
