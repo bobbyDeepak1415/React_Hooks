bfe919acd30a3ea7644394f8f941e04ae70070ce
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2026-09-23 01:38:34 +0530

    setup done

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import useFetchProds from './useFetchProds'
 
 const Demo = () => {
+
+  let url='https://dummyjson.com/products'
+
+  const {products}=useFetchProds(url)
+
   return (
     <div>
       Hello
--- src/useFetchProds.jsx
+++ src/useFetchProds.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const useFetchProds = () => {
+const useFetchProds = (url) => {
+
+  const [products,setProducts]=useState([])
+
+
+  useEffect(()=>{
+const fetchProds=async()=>{
+
+  const res=await fetch(url)
+  const response=await res.json()
+
+}
+
+fetchProds()
+  },[url])
+
   return (
     <div>
       Hello
